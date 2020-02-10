d7186de107f4cf480adf08d3bdcba82f12874e93
Piotr <Piotr.dangquoc@gmail.com>
2020-02-10 21:51:51 +0100

    Początek pracy
    działa malowanie modelu 3d, wybieranie koloru i zmiana wielkości pedzla

---
+++ Assets/Scripts/BrushManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushManager : MonoBehaviour
+{    
+    void Start()
+    {
+        //initializing values
+        Shader.SetGlobalColor("_BrushColor",Color.black);
+        Shader.SetGlobalFloat("_BrushOpacity",1);//
+        Shader.SetGlobalFloat("_BrushSize", 0.1f);
+        Shader.SetGlobalFloat("_BrushHardness", 1);
+    }
+
+    public void SetColor(string color)
+    {
+        switch(color)
+        {
+            case "red" : Shader.SetGlobalColor("_BrushColor",Color.red);
+            break;
+            case "blue" : Shader.SetGlobalColor("_BrushColor",Color.blue);
+            break;
+            case "green" : Shader.SetGlobalColor("_BrushColor",Color.green);
+            break;
+            case "rubber" : Shader.SetGlobalColor("_BrushColor",Color.white);
+            break;
+            case "black" : Shader.SetGlobalColor("_BrushColor",Color.black);
+            break;
+        }
+    }
+    public void SetBrushSize(float size)
+    {
+        Shader.SetGlobalFloat("_BrushSize", size);
+    }
+}
---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    private Vector2 hotSpot = Vector2.zero;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture,hotSpot,cursorMode);// changing cursor image
+    }
+}
